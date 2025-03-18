39861ff73974f1384e653f693582072e550417db
afadel151 <af8356207@gmail.com>
2025-03-18 22:12:03 +0100

    update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,10 +8,11 @@ import { FittingModule } from './fitting/fitting.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { Model3DService } from './model-3d/model-3d.service';
 import { Model3dModule } from './model-3d/model-3d.module';
+import { ClothingController } from './clothing/clothing.controller';
 
 @Module({
   imports: [AuthModule, UsersModule, ClothingModule, FittingModule, PrismaModule, Model3dModule],
-  controllers: [AppController],
+  controllers: [AppController,ClothingController],
   providers: [AppService,Model3DService, Model3DService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Nuxt frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // If you're using cookies or authentication tokens
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
